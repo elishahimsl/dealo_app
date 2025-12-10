7bfa05ee69bf543f760ea389afdfdd63e35b7589
Elisha Himsl <elishahimsl@gmail.com>
2025-12-10 01:18:54 +0000

    File changes

--- src/pages/StoreDetail.jsx
+++ src/pages/StoreDetail.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { createPageUrl } from "@/utils";
 import { Heart, Star, SlidersHorizontal, ChevronLeft, Menu, ShoppingBag } from "lucide-react";
 
 export default function StoreDetail() {
